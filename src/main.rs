e882dc36cc697e9ca4e8cb6e90d42506ac4c0458
Joaquin R <globojorro@gmail.com>
2017-06-26 12:15:11 -0500

    sensor readings every minute in csv-like format

--- src/main.rs
+++ src/main.rs
@@ -11,19 +11,14 @@ fn main() {
         let temp = sense_hat
             .get_temperature_from_humidity()
             .expect("Couldn't get temp");
-        println!("It's {} on the humidity sensor", temp);
-        let temp = sense_hat
+        let press_temp = sense_hat
             .get_temperature_from_pressure()
             .expect("Couldn't get temp");
-        println!("It's {} on the pressure sensor", temp);
         let rh = sense_hat.get_humidity().expect("Couldn't get rh");
-        println!("It's {} relative humidity", rh);
         let pressure = sense_hat.get_pressure().expect("Couldn't get pressure");
-        println!("The pressure is {}", pressure);
-        // let orientation = sense_hat
-        //     .get_orientation_degrees()
-        //     .expect("Couldn't get orientation");
-        // println!("The orientation is {:?}", orientation);
+        print!("'{:?}',", dt);
+        print!("'HUMIDITY: {} {}',", temp, rh);
+        println!("'PRESSURE: {} {}'", press_temp, pressure);
         ::std::thread::sleep(::std::time::Duration::from_millis(60000));
     }
 }
